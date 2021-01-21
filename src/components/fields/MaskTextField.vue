81243e962bfce1ccc126a0ffa4b7ec2dfb465525
nyaundi brian <danleyb2@gmail.com>
2021-01-21 21:03:58 +0300

    add input masks using MaskTextField

---
+++ src/components/fields/MaskTextField.vue
@@ -0,0 +1,80 @@
+<template>
+  <VTextField
+    v-model="internalValue"
+    :error-messages="errorMessages"
+    v-bind="$attrs"
+    @input="onInput"
+    @keyup="$emit('keyup', $event)"
+    @blur="$emit('blur', $event)"
+  />
+</template>
+
+<script>
+import { masker, tokens } from "./maskit"
+
+export default {
+  name: "MaskTextField",
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: String,
+      default: ""
+    },
+    errorMessages: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      internalValue: ""
+    }
+  },
+  computed: {
+    maskCharacters: function() {
+      let mask = this.$attrs.mask
+      if (mask) {
+        let preMade = null
+        switch (mask) {
+          case "phone":
+            preMade = "(###) ### - ####"
+            break
+          case "social":
+            preMade = "###-##-####"
+            break
+          default:
+            throw Error(`Unknown Mask: ${mask}`)
+        }
+        return preMade
+      }
+      return null
+    }
+  },
+  watch: {
+    value: {
+      handler: function(v) {
+        if (this.maskCharacters) {
+          this.internalValue = masker(v, this.maskCharacters, true, tokens)
+        } else {
+          this.internalValue = v
+        }
+      }
+    }
+  },
+  methods: {
+    onInput(newValue) {
+      this.$nextTick(() => {
+        let internalValue = newValue
+        let toEmit = newValue
+        if (this.maskCharacters) {
+          internalValue = masker(newValue, this.maskCharacters, true, tokens)
+          toEmit = masker(newValue, this.maskCharacters, false, tokens)
+        }
+
+        this.internalValue = internalValue
+        this.$emit("input", toEmit)
+      })
+    }
+  }
+}
+</script>
